18d8600dca90fe5ab6129db85f9898c2e85f53e9
Apple <webwerks@Apples-MacBook-Pro-3.local>
2019-08-13 14:54:14 +0530

    Carousel Completed

--- src/pages/DashboardScreen.js
+++ src/pages/DashboardScreen.js
@@ -1,17 +1,20 @@
 import React, { Component, } from 'react';
 import {
   View,
-  Text,
   ScrollView,
   Image,
   Dimensions,
   FlatList,
   TouchableOpacity,
   YellowBox,
-  AsyncStorage
+  AsyncStorage,
+  StyleSheet
 } from 'react-native';
 import { background } from '../components/slider';
+
+const DEVICE_WIDTH = Dimensions.get("window").width;
 export default class DashboardScreen extends Component {
+  scrollRef = React.createRef();
   static navigationOptions = ({ navigation }) => ({
     title: 'NeoSTORE',
     headerStyle: {
@@ -26,98 +29,142 @@ export default class DashboardScreen extends Component {
       token: ' ',
       fname: ' ',
       dataSource: [],
+      selectedIndex: 0,
     }
     this._retrieveData();
   }
 
-    _retrieveData = async () => {
-      try {
-        this.setState({
-          item: await AsyncStorage.getItem('@user_at'),
-          fname: await AsyncStorage.getItem('@user_fname')
-        })
-        console.log('\n *** HOME ***')
-      } catch (e) {
-        console.log(e);
-      }
-      console.log('Stored value : ', this.state.item, ' Fname ', this.state.fname);
+  _retrieveData = async () => {
+    try {
+      this.setState({
+        item: await AsyncStorage.getItem('@user_at'),
+        fname: await AsyncStorage.getItem('@user_fname')
+      })
+      console.log('\n *** HOME ***')
+    } catch (e) {
+      console.log(e);
     }
+    console.log('Stored value : ', this.state.item, ' Fname ', this.state.fname);
+  }
 
-    renderDots(){
-      return(
-        <View style={{ flex:0, flexDirection:'row', alignItems:'center',  }}>
-          <View style={{ flex:0, backgroundColor: 'gray', borderRadius: 8, width:8, height:8,marginRight:6,}}></View>
-          <View style={{ flex:0, backgroundColor: '#e8e3e3', borderRadius: 8, width:8, height:8,marginRight:6,}}></View>
-          <View style={{ flex:0, backgroundColor: '#e8e3e3', borderRadius: 8, width:8, height:8,marginRight:6,}}></View>
-          <View style={{ flex:0, backgroundColor: '#e8e3e3', borderRadius: 8, width:8, height:8,marginRight:6,}}></View>
-        </View>
-      )
-    }
+  componentDidMount = ()=> {
+    setInterval(() =>{
+      this.setState(prev =>({ selectedIndex: prev.selectedIndex === background.length - 1 ? 0 : prev.selectedIndex + 1 }),
+      ()=>{
+          this.scrollRef.current.scrollTo({
+            animated : true,
+            y: 0,
+            x: DEVICE_WIDTH * this.state.selectedIndex
+          })
+      })
+    },3000)
+  }
+  
+  setSelectedIndex = event => {
+    //width of the viewsize
+    const viewsize = event.nativeEvent.layoutMeasurement.width;
+    //get current position of scrollview
+    const contentOffset = event.nativeEvent.contentOffset.x;
+
+    const selectedIndex = Math.floor(contentOffset / viewsize);
+    this.setState({ selectedIndex : selectedIndex});
+  }
 
   render() {
     YellowBox.ignoreWarnings(["Warning: componentWillUpdate is deprecated"]);
     let dimensions = Dimensions.get("window");
     let imageHeight = Math.round((dimensions.width * 9) / 16);
     let imageWidth = dimensions.width;
-    let typewidth= dimensions.width/2.3;
-    let typeheight = dimensions.height/4.3;
+    let typewidth = dimensions.width / 2.3;
+    let typeheight = dimensions.height / 4.3;
     //console.log(dimensions.height+' '+typeheight);
-  
+    const selectedIndex = this.state.selectedIndex;
+    console.log('INDEX :'+selectedIndex);
+
 
 
     //const access_id= this.props.navigation.getParam("access_id",1);
 
     return (
 
-      <View style={{ flex: 0, alignItems: 'center', justifyContent: 'center',}}>
+      <View style={{ flex: 0, }}>
         <ScrollView
           horizontal={true}
           showsHorizontalScrollIndicator={true}
-          pagingEnabled= {true}
+          pagingEnabled={true}
           scrollEnabled={true}
           showsHorizontalScrollIndicator={false}
           decelerationRate={0}
           scrollEventThrottle={16}
           snapToAlignment="center"
+          onMomentumScrollEnd = {this.setSelectedIndex}
+          ref = {this.scrollRef}
           style={{
             height: 230,
           }}
         >
-          {background.map((item,index) => (
-            <View key ={`img-${index}`}>
+          {background.map((item, index) => (
+            <View key={`img-${index}`}>
               <Image
-              style={{ height: imageHeight, width: imageWidth }}
-              source={item.slider}
-            />
+                style={{ height: imageHeight, width: imageWidth }}
+                source={item.slider}
+              />
             </View>
           ))}
-              
-          </ScrollView>
-          {this.renderDots()}
-        
-        <FlatList
-          numColumns={2}
-          data={[
-            { key: 1, ur: require("../images/Table.png"), pth: 'Table' },
-            { key: 2, ur: require("../images/Sofas.png"), pth: 'Sofas' },
-            { key: 3, ur: require("../images/Chairs.png"), pth: 'Chairs' },
-            { key: 4, ur: require("../images/Cupboards.png"), pth: 'Cupboards' },
-          ]}
-          renderItem={({ item }) => {
-            // const p = item.pth;
-            return <TouchableOpacity  onPress={() => this.props.navigation.navigate(item.pth)}>
-              <Image
-                style={{ height: typeheight, width: typewidth, margin: 10, }}
-                source={item.ur} />
-            </TouchableOpacity>
-          }
-          }
-          keyExtractor={
-            (item, index) => index.toString()
-          }
-        />
+
+        </ScrollView>
+            {/* RENDER DOTS */}
+        <View style={styles.circleDiv}>
+          {background.map((item, i) => (
+            <View key={`item+${i}`} style={[styles.whiteCircle, { opacity: i == selectedIndex ? 0.5 : 1 }]}></View>
+          ))}
+        </View>
+
+        <View style={{ flex: 0, alignContent: 'center', justifyContent: 'center', width: '100%', marginLeft: 5, }}>
+          <FlatList
+            numColumns={2}
+            data={[
+              { key: 1, ur: require("../images/Table.png"), pth: 'Table' },
+              { key: 2, ur: require("../images/Sofas.png"), pth: 'Sofas' },
+              { key: 3, ur: require("../images/Chairs.png"), pth: 'Chairs' },
+              { key: 4, ur: require("../images/Cupboards.png"), pth: 'Cupboards' },
+            ]}
+            renderItem={({ item }) => {
+              // const p = item.pth;
+              return <TouchableOpacity onPress={() => this.props.navigation.navigate(item.pth)}>
+                <Image
+                  style={{ height: typeheight, width: typewidth, margin: 10, }}
+                  source={item.ur} />
+              </TouchableOpacity>
+            }
+            }
+            keyExtractor={
+              (item, index) => index.toString()
+            }
+          />
+        </View>
 
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  circleDiv: {
+    position: 'absolute',
+    width: '100%',
+    height: 415,
+    //bottom:50,
+    marginTop: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteCircle: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    margin: 5,
+    backgroundColor: 'white',
+  }
+})
